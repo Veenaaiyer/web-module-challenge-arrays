6bf3eecec82ded81d57a755701b7d221697b8767
Veena Aiyer <veenaaiyer@lambdasiyersmbp.lan>
2022-01-12 13:35:11 -0800

    done

--- index.js
+++ index.js
@@ -167,8 +167,14 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(array,item){
+  const newArray =[];
+  for(let i=0;i<array.length;i++){
+    if(array[i].includes(item)){
+      newArray.push(arrat[i]);
+    }
+  }
+  return newArray;
 }
 
 
